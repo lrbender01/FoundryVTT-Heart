1f980b83cf4c69e92dc226450aa5130f2d34dcd9
hitcherland <hitcherland@gmail.com>
2025-05-28 20:49:47 +0100

    Add haunt migration

--- module/data/migrations/item-haunt-migrations.mjs
+++ module/data/migrations/item-haunt-migrations.mjs
@@ -1,5 +1,18 @@
 function migration000(source) {
-  throw `Not implemented`;
+  const die_size_conversions = {
+    d4: 4,
+    d6: 6,
+    d8: 8,
+    d10: 10,
+    d12: 12,
+  };
+  let die_size = die_size_conversions[source.die_size || "d4"];
+  const resistances = source.resistances || [];
+
+  return {
+    die_size,
+    resistances
+  }
 }
 
 export default {
